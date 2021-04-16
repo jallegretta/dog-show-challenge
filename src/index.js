5ed7c021c386aac213e786631632ee59e593152a
Jared <jaralleg@amazon.com>
2021-04-16 17:29:43 -0400

    fetch function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    window.onload = (event) => {
+        fetch('http://localhost:3000/dogs') 
+            .then(response => response.json())
+            .then(data => console.log(data));
+      };
+    
 })
